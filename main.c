32fb934234ed663bd56f19341a0572ceec50fee4
Owen LEE <joker1251@naver.com>
2020-05-06 21:14:00 +0900

    test_code

--- main.c
+++ main.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
+#include "LIB_MAT.h"
 
 int main(int argc, char *argv)
 {
+    static LPF_PARA LPF_TEST;
+    int i=0;
+    int32_t j=0;
+
     printf("FIRST STEP TO GIT SUB!!\n");
+    
+    while(1)
+    {
+        LPF_TEST.flt_input = i++;
+        j=LIB_LPF(&LPF_TEST, 10,1);
 
+        printf("[%d]out test value is %lld \n",i,j);
 
+        if (i>10)
+            break;
+
+    }
 
     return 0;
 }
