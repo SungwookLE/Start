94f1ee318de752e2a78674d0c57bcfda33328d4a
Owen LEE <joker1251@naver.com>
2020-05-06 21:14:00 +0900

    test_code

---
+++ CMakeFiles/first.out.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/first.out.dir/LIB_MAT.c.o"
+  "CMakeFiles/first.out.dir/main.c.o"
+  "first.out.pdb"
+  "first.out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/first.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ typedef.h
@@ -0,0 +1,15 @@
+#ifndef _TYPEDEF_H_
+#define _TYPEDEF_H_
+
+    typedef long long int int32_t;
+    typedef long int int16_t;
+    typedef short int int8_t;
+    typedef unsigned long int uint16_t;
+    typedef unsigned short int uint8_t;
+    typedef unsigned long long int uint32_t;
+
+    typedef double double32_t;
+    
+
+
+#endif
